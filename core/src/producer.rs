0a826735257e9d2e6cf364a5a94bd6f6ffe2c640
kononnable <kononnable@gmail.com>
2021-01-23 15:32:11 +0100

    commit Cargo.lock

--- core/src/producer.rs
+++ core/src/producer.rs
@@ -1,9 +1,9 @@
-use crate::producer_options::ProducerOptions;
+use crate::{kafka_client::KafkaClient, producer_options::ProducerOptions};
 
 pub struct Producer {}
 
 impl Producer {
-    pub fn new(_brokers: &str, _options: ProducerOptions) -> Self {
+    pub fn new(_kafka_client: KafkaClient, _options: ProducerOptions) -> Self {
         Self {}
     }
 }
