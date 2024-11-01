4456b7814c28327747faf480c8cbcb5b808aaf8b
Kononnable <kononnable@gmail.com>
2024-11-01 09:18:30 +0000

    use Derivative for default configs

--- crates/client/src/cluster/options.rs
+++ crates/client/src/cluster/options.rs
@@ -1,26 +1,20 @@
+use derivative::Derivative;
 use std::time::Duration;
 
 // TODO: Rename fields?
 // TODO: add all options (from 1.0, without ones introduced later in KIPs to be implemented)
-#[derive(Clone, Debug)]
-pub struct ClusterControllerOptions {
-    pub connection_retires: u8,
-    pub connection_retry_delay: Duration,
-    pub connection_timeout: Duration,
-    pub request_timeout: Duration,
-    pub client_name: String,
-}
-
 // TODO: Set proper defaults
-// TODO: Use derivative or something similar? (for more readability)
-impl Default for ClusterControllerOptions {
-    fn default() -> Self {
-        ClusterControllerOptions {
-            connection_retires: 5,
-            connection_retry_delay: Duration::from_millis(1_000),
-            connection_timeout: Duration::from_secs(5),
-            request_timeout: Duration::from_secs(5),
-            client_name: "kafka-connector".to_owned(),
-        }
-    }
+#[derive(Clone, Debug, Derivative)]
+#[derivative(Default)]
+pub struct ClusterControllerOptions {
+    #[derivative(Default(value = "5"))]
+    pub connection_retires: u8,
+    #[derivative(Default(value = "Duration::from_millis(1_000)"))]
+    pub connection_retry_delay: Duration,
+    #[derivative(Default(value = "Duration::from_millis(5_000)"))]
+    pub connection_timeout: Duration,
+    #[derivative(Default(value = "Duration::from_millis(5_000)"))]
+    pub request_timeout: Duration,
+    #[derivative(Default(value = "\"kafka-connector\".to_owned()"))]
+    pub client_name: String,
 }
