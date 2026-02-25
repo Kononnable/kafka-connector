2c854b6c09081774ae23304764f97df6068d12aa
Kononnable <kononnable@gmail.com>
2026-02-25 01:11:00 +0100

    consumer loop refactor, list most common errors, no error handling yet

--- crates/client/src/clients/consumer/consumer_loop.rs
+++ crates/client/src/clients/consumer/consumer_loop.rs
@@ -7,17 +7,18 @@ use crate::cluster::error::ApiCallError;
 use crate::protocol_consts::{IsolationLevel, ListOffsetsTimestampType};
 use bytes::BytesMut;
 use futures::future::{join_all, select_all};
+use kafka_connector_protocol::ApiError;
 use kafka_connector_protocol::fetch_request::{FetchPartition, FetchRequest, FetchableTopic};
-use kafka_connector_protocol::fetch_response::{FetchResponse, FetchablePartitionResponse};
+use kafka_connector_protocol::fetch_response::FetchResponse;
 use kafka_connector_protocol::list_offset_request::{
     ListOffsetPartition, ListOffsetRequest, ListOffsetTopic,
 };
-use kafka_connector_protocol::list_offset_response::ListOffsetResponse;
 use kafka_connector_protocol::records::record_batch::RecordBatch;
 use std::collections::HashMap;
 use std::ops::Add;
 use std::pin::Pin;
 use std::sync::Arc;
+use std::task::{Context, Poll, Waker};
 use std::time::Duration;
 use tokio::sync::mpsc;
 use tracing::{debug, instrument};
@@ -26,7 +27,7 @@ pub struct ConsumerLoop {
     controller: Arc<ClusterController>,
     consumer_options: KafkaConsumerOptions,
     record_sender: mpsc::Sender<Record>,
-    command_receiver: mpsc::Receiver<()>,
+    reinitialize_triggered: bool,
     /// Broker -> Topic -> Partition -> next_offset_to_fetch, current_leader_epoch
     mappings: HashMap<i32, HashMap<String, HashMap<i32, (i64, i32)>>>,
     deserialization_buffer: BytesMut,
@@ -44,43 +45,23 @@ impl ConsumerLoop {
             controller,
             consumer_options,
             record_sender,
-            command_receiver,
+            reinitialize_triggered: false,
             mappings: Default::default(),
             deserialization_buffer: BytesMut::new(),
         }
-        .run()
+        .run(command_receiver)
         .await;
     }
 
     #[instrument(level = "debug", skip(self))]
-    async fn run(&mut self) {
+    async fn run(self, mut command_receiver: mpsc::Receiver<()>) {
         debug!("Consumer loop started.");
-        // TODO: Support fetching data from isr replicas
-        // TODO: Support metadata change self-healing - from time to time check metadata to see if partition count/broker isr have changed
 
-        self.prepare_mappings().await.unwrap(); // TODO:
-
-        // TODO: should be biased towards older futures - corner case: consumer taking long time to process messages,
-        //       fairness for consuming messages from different brokers
-        let mut requests_in_flight = Box::pin(select_all(
-            self.mappings
-                .keys()
-                .map(|broker_id| self.fetch_data(*broker_id))
-                .collect::<Vec<_>>(),
-        ));
-
-        loop {
-            tokio::select! {
-                biased;
-                None = self.command_receiver.recv() => {
-                    break;
-                }
-                ((broker_id, response),_, mut in_progress) = &mut requests_in_flight => {
-                    self.handle_response(broker_id, response).await;
-                    in_progress.push(self.fetch_data(broker_id));
-                    requests_in_flight = Box::pin(select_all(in_progress));
-                }
-            }
+        let mut main_loop = Box::pin(self.main_loop());
+        tokio::select! {
+            biased;
+            None = command_receiver.recv() => {}
+            _  = &mut main_loop => {}
         }
 
         debug!("Consumer loop is closing");
@@ -88,27 +69,94 @@ impl ConsumerLoop {
         // TODO: Make sure it closes when consumer client is dropped - test?
     }
 
-    async fn handle_response(
+    // TODO: Add tracing
+    async fn main_loop(mut self) -> ! {
+        let mut requests_in_flight = None;
+        loop {
+            if requests_in_flight.is_none() {
+                let refresh = if self.reinitialize_triggered {
+                    ForceRefresh::No
+                } else {
+                    ForceRefresh::Yes
+                };
+                self.reinitialize_triggered = false;
+                self.prepare_mappings(refresh).await.unwrap(); // TODO:
+                self.reinitialize_triggered = false;
+                requests_in_flight = Some(Box::pin(select_all(
+                    self.mappings
+                        .keys()
+                        .map(|broker_id| self.fetch_data(*broker_id))
+                        .collect::<Vec<_>>(),
+                )));
+            }
+
+            let ((broker_id, response), _, in_progress) = requests_in_flight.take().unwrap().await;
+            self.process_fetch_response(broker_id, response).await;
+
+            if self.reinitialize_triggered {
+                if !in_progress.is_empty() {
+                    requests_in_flight = Some(Box::pin(select_all(in_progress)));
+                }
+                continue;
+            }
+
+            // Manually pool all remaining futures before scheduling new ones - makes consumption from multiple brokers fair
+            let mut new_in_progress = vec![self.fetch_data(broker_id)];
+            for mut future in in_progress.into_iter() {
+                let pool_result = future
+                    .as_mut()
+                    .poll(&mut Context::from_waker(Waker::noop()));
+                match pool_result {
+                    Poll::Ready((broker_id, response)) => {
+                        self.process_fetch_response(broker_id, response).await;
+                        new_in_progress.push(self.fetch_data(broker_id));
+                    }
+                    Poll::Pending => {
+                        new_in_progress.push(future);
+                    }
+                }
+            }
+            requests_in_flight = Some(Box::pin(select_all(new_in_progress)));
+        }
+    }
+
+    async fn process_fetch_response(
         &mut self,
         broker: i32,
         response: Result<FetchResponse, ApiCallError>,
     ) {
         let response = response.unwrap(); // TODO:
-        assert!(response.error_code.is_none()); // TODO:
+        if let Some(error_code) = response.error_code {
+            match error_code {
+                _ => todo!("unknown error"),
+            }
+        }
         for topic_response in response.topics {
             let topic = topic_response.name.as_str();
             for partition_response in topic_response.partitions {
-                assert!(partition_response.error_code.is_none()); // TODO:
+                if let Some(error_code) = response.error_code {
+                    match error_code {
+                        ApiError::UnknownTopicOrPartition => todo!(),
+                        ApiError::NotLeaderOrFollower => todo!(),
+                        ApiError::ReplicaNotAvailable => todo!(),
+                        ApiError::FencedLeaderEpoch => todo!(),
+                        ApiError::UnknownLeaderEpoch => todo!(),
+                        ApiError::OffsetOutOfRange => todo!(),
+                        ApiError::CorruptMessage => todo!(),
+                        ApiError::RequestTimedOut => todo!(),
+                        _ => todo!("unknown error"),
+                    }
+                }
                 let partition = partition_response.partition_index;
                 if let Some(records) = partition_response.records {
-                    self.handle_partition(broker, topic, partition, records)
+                    self.process_partition_records(broker, topic, partition, records)
                         .await;
                 }
             }
         }
     }
 
-    async fn handle_partition(
+    async fn process_partition_records(
         &mut self,
         broker: i32,
         topic: &str,
@@ -143,7 +191,7 @@ impl ConsumerLoop {
                     offset,
                 };
 
-                let _ = self.record_sender.send(record).await; // TODO: this await may block
+                let _ = self.record_sender.send(record).await;
             }
         }
         if let Some(offset) = last_processed_offset {
@@ -179,12 +227,12 @@ impl ConsumerLoop {
                         name: topic.to_owned(),
                         fetch_partitions: partitions
                             .iter()
-                            .map(|(partition_index, next_offset)| {
+                            .map(|(partition_index, &(offset, epoch))| {
                                 FetchPartition {
                                     partition_index: *partition_index,
-                                    // TODO: pass leader epoch, handle errors in response - UNKNOWN_LEADER_EPOCH, FENCED_LEADER_EPOCH - force metadata refresh, check if offset exists (records may disappear if unclean leader election is on), send standard fetch requests
-                                    current_leader_epoch: 0,
-                                    fetch_offset: next_offset.0,
+                                    // TODO: handle errors in response - UNKNOWN_LEADER_EPOCH, FENCED_LEADER_EPOCH - force metadata refresh, check if offset exists (records may disappear if unclean leader election is on), send standard fetch requests
+                                    current_leader_epoch: epoch,
+                                    fetch_offset: offset,
                                     log_start_offset: -1,
                                     max_bytes: 1048576, // TODO: 1MB, librdkafka defaults
                                 }
@@ -205,29 +253,48 @@ impl ConsumerLoop {
         })
     }
 
-    async fn prepare_mappings(&mut self) -> Result<(), ConsumeError> {
-        assert!(self.mappings.is_empty());
-
+    async fn prepare_mappings(&mut self, refresh: ForceRefresh) -> Result<(), ConsumeError> {
         for (topic, metadata) in self
             .controller
-            .get_topic_metadata([self.consumer_options.topic.clone()], ForceRefresh::No)
+            .get_topic_metadata([self.consumer_options.topic.clone()], refresh)
             .await
             .map_err(MetadataFetchFailed)?
             .into_iter()
         {
-            assert!(metadata.error_code.is_none()); // TODO:
+            if let Some(error_code) = metadata.error_code {
+                match error_code {
+                    ApiError::UnknownTopicOrPartition => {
+                        // remove old mappings if topic was removed
+                        self.mappings.iter_mut().for_each(|(_, mappings)| {
+                            mappings.remove(&topic);
+                        });
+                        todo!("log error");
+                    }
+                    ApiError::InvalidTopicException => {
+                        todo!("log error")
+                    }
+                    _ => todo!("unknown error"),
+                }
+            }
+
             for partition_metadata in metadata.partitions {
-                assert!(partition_metadata.error_code.is_none()); // TODO:
-                self.mappings
+                if let Some(error_code) = partition_metadata.error_code {
+                    todo!("unknown error")
+                }
+
+                let (_, epoch) = self
+                    .mappings
                     .entry(partition_metadata.leader_id)
                     .or_default()
                     .entry(topic.clone())
                     .or_default()
-                    .insert(
-                        partition_metadata.partition_index,
-                        (-1, partition_metadata.leader_epoch),
-                    );
+                    .entry(partition_metadata.partition_index)
+                    .or_insert((-1, -1));
+                *epoch = partition_metadata.leader_epoch;
             }
+        }
+        if self.mappings.is_empty() {
+            todo!();
         }
 
         let list_offset_responses = join_all(self.mappings.iter().map(|(broker_id, mappings)| {
@@ -241,9 +308,9 @@ impl ConsumerLoop {
                             name: topic.to_owned(),
                             partitions: partitions
                                 .iter()
-                                .map(|(partition, &(_, current_leader_epoch))| {
+                                .map(|(&partition_index, &(_, current_leader_epoch))| {
                                     ListOffsetPartition {
-                                        partition_index: *partition,
+                                        partition_index,
                                         current_leader_epoch, // TODO: Handle errors -  UNKNOWN_LEADER_EPOCH, FENCED_LEADER_EPOCH
                                         timestamp: ListOffsetsTimestampType::Earliest.into(), // TODO: value from options
                                         max_num_offsets: 0,
@@ -255,16 +322,11 @@ impl ConsumerLoop {
                     .collect(),
             };
 
-            return send_list_offsets_request(*broker_id, &self.controller, request);
-
-            async fn send_list_offsets_request(
-                broker_id: i32,
-                controller: &Arc<ClusterController>,
-                request: ListOffsetRequest,
-            ) -> (i32, Result<ListOffsetResponse, ApiCallError>) {
+            let controller = self.controller.clone();
+            async move {
                 (
-                    broker_id,
-                    controller.make_api_call(broker_id, request, None).await,
+                    *broker_id,
+                    controller.make_api_call(*broker_id, request, None).await,
                 )
             }
         }))
@@ -275,14 +337,23 @@ impl ConsumerLoop {
             let broker_mappings = self.mappings.entry(broker_id).or_default();
             for topic in response.topics {
                 for partition in topic.partitions {
-                    assert!(partition.error_code.is_none()); // TODO:
-                    broker_mappings
+                    if let Some(error_code) = partition.error_code {
+                        match error_code {
+                            ApiError::FencedLeaderEpoch => todo!(),
+                            ApiError::BrokerNotAvailable => todo!(),
+                            _ => todo!("Unknown error"),
+                        }
+                    }
+                    let (offset, _) = broker_mappings
                         .entry(topic.name.clone())
                         .or_default()
-                        .insert(
-                            partition.partition_index,
-                            (partition.offset, partition.leader_epoch),
-                        );
+                        .entry(partition.partition_index)
+                        .or_insert((-1, partition.leader_epoch));
+                    if *offset == -1 || (*offset < partition.offset && true) {
+                        // TODO: true - Earliest timestamp from (options)
+                        // TODO: make sure corer case works - timestamp !=earliest and consumer fallen behind retention (data deleted)
+                        *offset = partition.offset;
+                    }
                 }
             }
         }
